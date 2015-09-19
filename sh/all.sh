20247ca3a2a736284a895c342a8df26c55f247db
Marek Dudek <marek.dudek@gmail.com>
2015-09-19 12:11:12 +0200

    moved obsolete scripts to subdirectory

--- sh/all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./sh/example-spec.sh
-./sh/styles.sh
-./sh/assertions.sh
---
+++ sh/all/all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./sh/all/example-spec.sh
+./sh/all/styles.sh
+./sh/all/assertions.sh
